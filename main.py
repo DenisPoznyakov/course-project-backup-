b64e5399b148132ed03960a8a84ac11f8efa4ece
Denis Poznyakov <flint037@gmail.com>
2023-12-06 17:13:52 +0300

    commit

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import requests
 from pprint import pprint
-from urllib.parse import urlencode
+import os
+import json
 
 TOKEN_VK = 'vk1.a.EsVK4t3jGAAYz1i84jpMnj0DuPN-GQ6j77mK9jXIx4fvl0YOt9We6I_dlXYa0JHrtsafkz5SQXxdzb6akBeWi3SoePAXiVamT3QtitVmOefahsLt9yIukTxpMu25X6a1KcjWsre_ZUNlbglJlFwT1_w7AQjRDBapsEqXpauo-Nu7CTSlEc8qScGnqr4DXG1fEZmO6L1EZ46nYtVyQPzWQA'
 TOKEN_YA = 'OAuth y0_AgAAAAAbF_21AADLWwAAAADyrrtr6Xg3WBLMSjqjbfp_kikw3TPLVbs'
+
+def create_json():
+    json_data = [
+    ]
+    with open('log.json', 'w') as file:
+        file.write(json.dumps(json_data, indent=2, ensure_ascii=True))
+
+create_json()
+
+def add_to_json(par_1, par_2):
+    json_data = {
+        par_1: par_2
+    }
+    data = json.load(open("log.json"))
+    data.append(json_data)
+    with open("log.json", "w") as file:
+        json.dump(data, file, indent=2, ensure_ascii=True)
 
 class VK:
 
@@ -20,23 +38,27 @@ class VK:
         }
 
     def get_profile_photos(self):
+        if not os.path.exists('backup photos VK'):
+            os.mkdir('backup photos VK')
+            add_to_json('backup photos VK', 'a folder has been created on the local disk')
         max_size_photo = {}
         params = self.get_common_params_vk()
         params.update({'owner_id': self.user_id, 'album_id': 'profile', 'extended': '1'})
         response = requests.get(f'{self.API_BASE_URL_VK}/photos.get', params=params)
         for photo in response.json()['response']['items']:
             max_size = 0
-            # photos_info = {}
             for size in photo['sizes']:
                 if size['height'] >= max_size:
                     max_size = size['height']
                     if photo['likes']['count'] not in max_size_photo.keys():
                         max_size_photo[photo['likes']['count']] = size['url']
-                        # photos_info['file_name'] = f"{photo['likes']['count']}.jpg"
                     else:
                         max_size_photo[f"{photo['likes']['count']} + {photo['date']}"] = size['url']
-                        # photos_info['file_name'] = f"{photo['likes']['count']}+{photo['date']}.jpg"
-        return max_size_photo
+        for photo_name, photo_url in max_size_photo.items():
+            with open('backup photos VK/%s' % f'{photo_name}.jpg', 'wb') as file:
+                img = requests.get(photo_url)
+                file.write(img.content)
+                add_to_json(photo_name, 'downloaded to local disk')
 
 class YA:
 
@@ -51,6 +73,7 @@ class YA:
                    'Authorization': TOKEN_YA}
         params = {'path': 'backup photos VK'}
         response = requests.put(url=url, headers=headers, params=params)
+        add_to_json('backup photos VK', 'a folder has been created on Yandex Disk')
 
     def upload(self, file_path: str):
         url = self.API_BASE_URL_YA + '/v1/disk/resources/upload'
@@ -62,10 +85,7 @@ class YA:
         response = requests.get(url=url, headers=headers, params=params)
         href = response.json().get('href')
 
-        for file in vk_client.get_profile_photos():
-            uploader = requests.put(href, data=photos_info.values())
-
-        # uploader = requests.put(href, data=open(files_path, 'rb'))
+        uploader = requests.put(href, data=open(files_path, 'rb'))
 
 if __name__ == '__main__':
     vk_client = VK(TOKEN_VK, 151422792)
@@ -74,9 +94,9 @@ if __name__ == '__main__':
     ya_client = YA(TOKEN_YA)
     ya_client.folder_creation()
 
-    for photo in photos_info:
-        file_name = photos_info.keys()
-        files_path = photos_info.values()
-        # result = ya_client.upload(files_path)
-        # print(f'Фотография {photos_info.keys()} загружена на Яндекс диск')
-        # print(photos_info.keys())
+    photos_list = os.listdir('backup photos VK')
+    for photo in photos_list:
+        file_name = photo
+        files_path = os.getcwd() + r'\backup photos VK\\' + photo
+        result = ya_client.upload(files_path)
+        add_to_json(photo, 'uploaded to Yandex Disk')
